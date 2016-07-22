5d5b6cab16c2167e2874e7ca590f7af1cc6c853c
Peter Meissner <retep.meissner@gmail.com>
2016-07-22 14:28:23 +0200

    text_dup text_rep

---
+++ R/text_dup.R
@@ -0,0 +1,67 @@
+#' generic repeating text
+#' @param string text to be repeated
+#' @param times how many times shal string be repeated
+#' @param vectorize should function be used in vectorized mode, i.e. should a
+#'    pattern with length larger than 1 be allowed and if so, should it be
+#'    matched to lines (with recycling if needed) instead of using on element on
+#'    all lines
+#' @param ... further arguments passed through
+#' @export
+text_rep <- function(string, times, vectorize=FALSE, ...){
+  UseMethod("text_rep")
+}
+
+#' @export
+#' @rdname text_rep
+text_dup <- function(string, times, vectorize=FALSE, ...){
+  UseMethod("text_rep")
+}
+
+
+
+#' text_rep defaul method
+#' @rdname text_rep
+#' @method text_rep default
+#' @export
+text_rep.default <- function(string, times, vectorize=FALSE, ...){
+  if(is.list(string)){
+    tmp  <- lapply(string, text_rep, times=times, vectorize=vectorize, ...)
+    return(tmp)
+  }
+  if(vectorize){
+    tmp <- mapply(text_rep, string, times)
+    names(tmp) <- NULL
+    tomp <-
+      as.data.frame(
+        do.call(
+          rbind,
+          mapply(c,i=seq_along(string), p=seq_along(times), SIMPLIFY = FALSE)
+        )
+      )
+    tmp <- data.frame(t=tmp, i=tomp$i, p=tomp$p)
+    rownames(tmp) <- NULL
+    return(tmp)
+  }else{
+    tmp <-
+      vapply(
+        X   = string,
+        FUN =
+          function(string, times){
+            paste0(rep(string, times), collapse="")
+          },
+        FUN.VALUE = "",
+        times = times
+      )
+    return(tmp)
+  }
+}
+
+
+
+
+
+
+
+
+
+
